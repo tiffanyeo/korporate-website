fbac98c9e1f26a74e2d454fbbc77d7da6034771d
sjfalina <alinab91@hotmail.com>
2026-04-21 11:30:04 +0200

    sub-nav fix

--- client/components/sub-navigation/sub-navigation.js
+++ client/components/sub-navigation/sub-navigation.js
@@ -1,6 +1,6 @@
 /*
 Exempel på hur man använder sub-navigationen
-<sub-nav id="myNav"></sub-nav>
+<sub-navigation id="myNav"></sub-navigation>
 
 <script>
   document.getElementById('myNav').buttons = [
@@ -17,19 +17,23 @@ export class SubNavigation extends HTMLElement{
     constructor(){
         super();
         this.attachShadow({ mode: "open" });
-        this._buttons;
+        this._buttons = [];
 
     }
 
     set buttons(value){
         this._buttons = value;
         this.render();
+        this.eListeners();
     }
 
     renderButtons(){
-        let html = ``;
-        this._buttons.forEach(link => html.push(`<button>${link.title}</button>`));
-        return html;
+        if (!this._buttons) return "";
+        return this._buttons
+        .map((link, index) =>
+            `<button ${index === 0 ? 'id="active"' : ''}>${link.title}</button>`
+        )
+        .join("");
     }
 
     connectedCallback(){
@@ -49,8 +53,8 @@ export class SubNavigation extends HTMLElement{
         this.shadowRoot.innerHTML =`
         <style>
             .sub-nav{
-                width: 500px;
-                height:fit-content;
+                width: 300px;
+                height:40px;
                 display: flex;
                 gap: 8px;
                 margin: 0;
@@ -58,16 +62,18 @@ export class SubNavigation extends HTMLElement{
             }
 
             .sub-nav button{
+                border: 1px solid var(--color-gray-30);
                 font-family: Arial, Helvetica, sans-serif;
                 font-size: 14px;
-                background-color: white;
+                background-color: var(--color-gray-100);
                 border-radius: 16px 16px 0 0;
                 box-shadow: inset 0 -1em 2em rgb(0 0 0 / 30%),
                 -0.1em -0.1em 0.5em rgba(0, 0, 0, 0.134);
                 border-bottom: none;
-                width: 80px;
-                height: 16px;
-                padding: 4px 8px;
+                min-width: 80px;
+                max-width: fit-content;
+                height: 30px;
+                padding: 4px 16px;
                 text-align: center;
             }
 
