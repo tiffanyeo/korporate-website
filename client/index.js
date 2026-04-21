8cc6e4ad0b69dd8f6de89be4addece05bfb09624
Tiffany <tiffanylarsson.stud@gmail.com>
2026-04-21 14:03:19 +0200

    chore: add staff component

--- client/index.js
+++ client/index.js
@@ -4,6 +4,8 @@ import "./components/header-elem/header-elem.js";
 import "./components/main-navigation/main-navigation.js";
 import "./components/sub-navigation/sub-navigation.js";
 
+// COMPONENTS VIEW SPECIFIC (move later?)
+import "./views/home/components/staff-card-container-elem/staff-card-container-elem.js"
 
 window.addEventListener("DOMContentLoaded", () => {
     document.getElementById('myNav').buttons = [
