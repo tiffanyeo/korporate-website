c062c30df15f9a043fd4da772ede5928004d0050
sjfalina <alinab91@hotmail.com>
2026-04-21 11:30:12 +0200

    test sub-nav

--- client/index.js
+++ client/index.js
@@ -1,4 +1,12 @@
 
 // COMPONENTS 
-import "./components/header-elem/header-elem.js"
-import "./components/main-navigation/main-navigation.js"
+import "./components/header-elem/header-elem.js";
+import "./components/main-navigation/main-navigation.js";
+import "./components/sub-navigation/sub-navigation.js";
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById('myNav').buttons = [
+        { title: 'Start', href: '/' },
+        { title: 'Våra tjänster' }
+    ]});
