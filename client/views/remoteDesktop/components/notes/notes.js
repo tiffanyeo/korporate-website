d3a55c025f5c2c2da06ec840b339ae18d1e91091
sjfalina <alinab91@hotmail.com>
2026-05-07 21:49:44 +0200

    Notes: Removed unnecessary placeholder

--- client/views/remoteDesktop/components/notes/notes.js
+++ client/views/remoteDesktop/components/notes/notes.js
@@ -43,7 +43,7 @@ export class NotesApp extends HTMLElement{
                     </div>
                     <div class="window-body">
                     <div id="input-box">
-                        <textarea name="" id="addText" cols="38" rows="10" placeholder="I"></textarea>
+                        <textarea name="" id="addText" cols="38" rows="10" placeholder=""></textarea>
                     </div>
                   </div>
                 </div>
