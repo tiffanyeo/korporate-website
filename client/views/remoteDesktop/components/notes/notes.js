c58132ce0c727a19f1f7fe3bd46d6d6079a5036b
sjfalina <alinab91@hotmail.com>
2026-05-11 18:05:56 +0200

    Fix Safari compatibility
    Remove css import attributes

--- client/views/remoteDesktop/components/notes/notes.js
+++ client/views/remoteDesktop/components/notes/notes.js
@@ -1,5 +1,3 @@
-import sheet from "./notes.css" with {type: "css"};
-
 export class NotesApp extends HTMLElement{
     constructor(){
         super();
@@ -8,8 +6,6 @@ export class NotesApp extends HTMLElement{
 
     connectedCallback(){
         this.render();
-
-        this.shadowRoot.adoptedStyleSheets = [sheet];
         this.setElistener();
     }
 
@@ -38,6 +34,137 @@ export class NotesApp extends HTMLElement{
     render(){
         this.shadowRoot.innerHTML = `
             <style>
+                .window {
+                margin: 8px;
+                width: fit-content;
+                font-size: 11px;
+                border: 2px solid rgba(0, 102, 255, 1);
+                background: lightblue;
+                display: flex;
+                flex-direction: column;
+                border-radius: 4px;
+            }
+
+            .window fieldset {
+                margin-bottom: 9px;
+            }
+
+            .title-bar {
+
+                font-size: 11px;
+                background: linear-gradient(180deg,
+                                rgba(9, 151, 255, 1) 0%,
+                                rgba(0, 83, 238, 1) 8%,
+                                rgba(0, 80, 238, 1) 40%,
+                                rgba(0, 102, 255, 1) 88%,
+                                rgba(0, 102, 255, 1) 93%,
+                                rgba(0, 91, 255, 1) 95%,
+                                rgba(0, 61, 215, 1) 96%,
+                                rgba(0, 61, 215, 1) 100%);
+                padding: 3px 2px 3px 3px;
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+
+            }
+            .title-bar-text {
+                font-weight: bold;
+                color: white;
+                letter-spacing: 0;
+                margin-right: 24px;
+            }
+            .title-bar-controls {
+                display: flex;
+                gap: 2px;
+            }
+                
+            .title-bar-controls button {
+                padding: 0;
+                display: block;
+                min-width: 14px;
+                min-height: 14px;
+                border: white;
+                border-radius: 2px;
+            }
+                
+            .title-bar-controls button:focus {
+                outline: none;
+            }
+                
+            button[aria-label="Minimize"] {
+                background-image: url(../../../assets/icons/MinimizeWindow.png);
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            button[aria-label="Maximize"] {
+                background-image: url(../../../assets/icons/MaximizeWindow.png);
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            button[aria-label="Close"] {
+                background-image: url(../../../assets/icons/CloseWindow.png);
+                background-position: center;
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+                
+                
+            .window-menu{
+                padding: 2px 4px;
+                background: #dfdfdf;
+                box-shadow: 0 0 1px rgba(0, 0, 0, 0.18);
+                display: flex;
+                gap: 8px;
+                
+            }
+                
+            .window-menu-item {
+                font-size: 10px;
+            }
+                
+            .window-body {
+                overflow: clip;
+                flex-grow: 1;
+                border: rgb(0, 61, 215);
+            }
+                
+            .window-body pre {
+                margin: -8px;
+            }
+                
+            textarea{
+                border: 0.5px solid #808080;
+                min-width: 180px;
+                max-width: 400px;
+            }
+                
+                
+            .status-bar {
+                margin: 0px 1px;
+                display: flex;
+                gap: 1px;
+            }
+                
+            .status-bar-field {
+                box-shadow: inset -1px -1px;
+                flex-grow: 1;
+                padding: 2px 3px;
+                margin: 0;
+            }
+                
+                
+            :host(.maximized) #notes {
+                position: fixed;
+                width: 100%;
+                height: 100%;
+            }
+                
+            :host(.minimized) #notes {
+                height: 30px;
+                overflow: hidden;
+            }
             </style>
             <div id="notes">
                 <div class="window">
