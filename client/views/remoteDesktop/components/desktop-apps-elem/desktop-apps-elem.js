71586a56f3b3c32195ffa21f69aa1f91add5b785
sjfalina <alinab91@hotmail.com>
2026-05-11 20:31:55 +0200

    desktop-icon component
    For adding icons to desktop

---
+++ client/views/remoteDesktop/components/desktop-apps-elem/desktop-apps-elem.js
@@ -0,0 +1,72 @@
+export class DesktopElement extends HTMLElement{
+    connectedCallback(){
+        this.name = this.getAttribute("name") || "Unnamed";
+        this.imgpath = this.getAttribute("imgpath") || "../../assets/icons/app-icon-folder.png";
+        this.app = this.getAttribute("app") || "none";
+        this.render();
+    }
+
+    eListeners(){
+        const root = this.getRootNode();
+        const content = root.querySelector("#content");
+
+        const desktop = root.querySelector(".desktop");
+        const icons = desktop.querySelectorAll("desktop-element");
+        icons.forEach(icon => icon.addEventListener("click", ()=>{
+            const app = icon.getAttribute("app");
+            if (app === "notes") {
+                console.log("rätt!")
+                content.appendChild(document.createElement("notes-app"));
+            }
+
+            if (app === "explorer") {
+                content.appendChild(document.createElement("explorer-app"));
+            }
+        }))
+
+    }
+
+    render(){
+        this.innerHTML = `
+            <style>
+                .desktop-icon {
+                    height: 80px;
+                    width: 80px;
+                    padding: 8px;
+                    align-items: center;
+                    border: 1px solid transparent;
+                    border-radius: 3px;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: center;
+                    position: relative;
+                    z-index: 5;
+                    cursor: default;
+                }
+                
+                .desktop-icon:hover {
+                    background: rgba(255, 255, 255, 0.21)
+                }
+
+                .desktop-icon img {
+                    width: 60px;
+                    height: 60px;
+                }
+
+                .desktop-icon span {
+                    margin-top: 4px;
+                    text-align: center;
+                    color: white;
+                    text-shadow: 1px 1px 2px black;
+                }
+            </style>
+
+            <div class="desktop-icon">
+                <img src="${this.imgpath}">
+                <span>${this.name}</span>
+            </div>
+        `;
+        this.eListeners();
+    }
+}
+customElements.define("desktop-element", DesktopElement);
