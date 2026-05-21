e0f862fea5c175deb38ea650879ea8e016d62872
Tiffany <tiffanylarsson.stud@gmail.com>
2026-05-21 14:59:54 +0200

    feat: add qr view

--- client/dev.js
+++ client/dev.js
@@ -4,6 +4,7 @@ import "./views/intranet/intranet.js"
 import "./views/news/news.js"
 import "./views/contact/contact.js"
 import "./views/remoteDesktop/remoteDesktop.js"
+import "./views/qr/qr.js"
 
 // COMPONENTS 
 import "./components/header-elem/header-elem.js";
@@ -11,7 +12,7 @@ import "./components/main-navigation/main-navigation.js";
 import "./components/sub-navigation/sub-navigation.js";
 
 import "./views/staffIntranet/components/sign-out-elem/sign-out-elem.js"
-import "./views/staffIntranet/components/billboard/billboard.js"
+
 
 // COMPONENTS VIEW SPECIFIC (move later?)
 import "./components/staff-card-container-elem/staff-card-container-elem.js"
---
+++ qr/dev.js
@@ -0,0 +1,11 @@
+
+// EVENT-LISTEBER
+const btnTrashCan = this.shadowRoot.querySelector(".choice1");
+btnTrashCan.addEventlistener("click", () => {
+    // load html 1
+})
+
+const btnMailBox = this.shadowRoot.querySelector(".choice1");
+btnMailBox.addEventlistener("click", () => {
+    // load html 2
+})
